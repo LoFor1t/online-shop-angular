c89a542ccd131efef234fc4bb39825ae5cdbea9c
Vlad Hrieshnykh <vgresnyh@gmail.com>
2024-02-12 14:30:24 +0100

    Moved recipes array to recipe service.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,23 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simple a test', 'https://tech.caltech.edu/img/2023/mar03/cooking.jpg'),
-    new Recipe('Another Test Recipe', 'This is simple a test', 'https://tech.caltech.edu/img/2023/mar03/cooking.jpg'),
-    new Recipe('A Test Recipe', 'This is simple a test', 'https://tech.caltech.edu/img/2023/mar03/cooking.jpg')
-  ];
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) {
+  }
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {Recipe} from "./recipe.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simple a test', 'https://tech.caltech.edu/img/2023/mar03/cooking.jpg'),
+    new Recipe('Another Test Recipe', 'This is simple a test', 'https://tech.caltech.edu/img/2023/mar03/cooking.jpg'),
+  ];
+
+  constructor() {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
