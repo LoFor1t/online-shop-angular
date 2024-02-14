0074540edee22eb1b47bdcc65147931691216fb8
Vlad Hrieshnykh <vgresnyh@gmail.com>
2024-02-14 11:49:44 +0100

    Added array of ingredients to Recipe model. Added functionality for adding all ingredients from recipe to shopping list.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,22 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simple a test', 'https://tech.caltech.edu/img/2023/mar03/cooking.jpg'),
-    new Recipe('Another Test Recipe', 'This is simple a test', 'https://tech.caltech.edu/img/2023/mar03/cooking.jpg'),
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meet', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meet', 1)
+      ]),
   ];
 
   constructor() {
